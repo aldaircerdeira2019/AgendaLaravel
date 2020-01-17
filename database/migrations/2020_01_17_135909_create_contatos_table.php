f98d2e2ea2c0307461fe17b0739943ef9de486c1
aldair cerdeira <aldaircerdeira10@hotmail.com>
2020-01-17 15:11:29 -0400

    criação da  migrate e model da entidade contatos

---
+++ database/migrations/2020_01_17_135909_create_contatos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome',60);
+            $table->char('telefone',11);
+            $table->string('email',100)->nullable();
+            $table->date('data_n')->nullable();
+            $table->text('descrição')->nullable();
+            $table->string('avatar',160)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contatos');
+    }
+}
